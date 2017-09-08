600814c0bcc57bdb578dea5e54ca35d87e0231ea
LiveTeXon <30394002+LiveTeXon@users.noreply.github.com>
2017-09-08 21:44:30 +0200

    New

--- src/de/weckstyapi/utils/NewNickAPI.java
+++ src/de/weckstyapi/utils/NewNickAPI.java
@@ -12,6 +12,36 @@ public class NewNickAPI {
 
     static {
         Names.add("AirFusion");
+        Names.add("AmazingHuh");
+        Names.add("aranamor");
+        Names.add("atomic7732");
+        Names.add("Carmelpop");
+        Names.add("catlover2011");
+        Names.add("Chan14551");
+        Names.add("Zoarium");
+        Names.add("Chzydeath");
+        Names.add("CoolBlueJ");
+        Names.add("DaBomb");
+        Names.add("Darvince");
+        Names.add("DaSnipeKid");
+        Names.add("Dawnofdusk");
+        Names.add("De_n00bWOLF");
+        Names.add("DeepDarkSamu");
+        Names.add("DefaultStu");
+        Names.add("diamond146");
+        Names.add("DirtDog");
+        Names.add("DivinityV2");
+        Names.add("F0R1");
+        Names.add("faloxx");
+        Names.add("fire3232");
+        Names.add("firehawk729");
+        Names.add("Firestix");
+        Names.add("Fixin");
+        Names.add("For_the_lol");
+        Names.add("FoxHound42");
+        Names.add("Entophobia");
+        Names.add("Erak606");
+        Names.add("Peaa");
     }
     
     public static void setNick(final Player p) {
